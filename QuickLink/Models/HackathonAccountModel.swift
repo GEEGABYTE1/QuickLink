64654e8476a119ac98d279cea36907030c7a0d2a
Jaival Patel <pateljaival05@gmail.com>
2022-07-25 15:01:42 -0400

    Hackathon Beginning Startup

---
+++ QuickLink/Models/HackathonAccountModel.swift
@@ -0,0 +1,15 @@
+//
+//  HackathonAccountModel.swift
+//  QuickLink
+//
+//  Created by Jaival Patel on 2022-07-25.
+//
+
+import Foundation
+
+struct HackathonAccount {
+    
+    
+    
+    
+}
--- QuickLink/Views/HacakthonAccountCreationView.swift
+++ QuickLink/Views/HacakthonAccountCreationView.swift
@@ -8,10 +8,50 @@
 import SwiftUI
 
 struct HackathonCreationView: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
     var body: some View {
+        
         ZStack {
-            Text("Hackathon Creation")
+            AppColor.wall_color.ignoresSafeArea()
+            VStack {
+
+                Text("Hackathon Account Creation")
+                    .font(.title)
+                    .padding()
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    
+            Spacer()
+             
+            TextField("Username", text: $username)
+                .padding()
+                .background(AppColor.box_color)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+                
+            SecureField("Password", text: $username)
+                .padding()
+                .background(AppColor.box_color)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+                
+            Text("Create Your Account")
+                    .padding()
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 220, height: 60)
+                    .background(Color.green)
+                    .cornerRadius(15.0)
+                
+            Spacer()
+            }.padding()
+            
+            
         }  .navigationBarBackButtonHidden(true)
+            
         
     }
         
